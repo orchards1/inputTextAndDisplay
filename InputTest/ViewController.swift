e33cd14d8e06ce1bf5f2cdce241bc1c01c9b45f7
Michael <jagosikat@yahoo.co.id>
2019-05-16 15:23:59 +0700

    Initial Commit

---
+++ InputTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InputTest
+//
+//  Created by Michael Louis on 16/05/19.
+//  Copyright © 2019 Michael Louis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
